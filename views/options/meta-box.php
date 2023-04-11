95ad243e5ce4920e27e148235037f63778ccf98d
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2023-04-11 11:37:22 +0700

    Update conflict js

--- views/options/meta-box.php
+++ views/options/meta-box.php
@@ -3,18 +3,18 @@ if (!defined('ABSPATH')) {
     exit;
 }
 ?>
-<div id="nbo-wraper">
+<div id="printcart-wraper">
     <?php wp_nonce_field('pc_box', 'pc_box_nonce'); ?>
     <div style="overflow: hidden;">
-        <div class="nbo_options_panel" id="nbo-options">
-            <p class="nbo-form-field">
+        <div class="printcart_options_panel" id="printcart-options">
+            <p class="printcart-form-field">
                 <label for="_printcart_pb_enable"><?php _e('Enable Product builder', 'pc-product-builder'); ?></label>
-                <span class="nbo-option-val">
+                <span class="printcart-option-val">
                     <input type="hidden" value="0" name="_printcart_pb_enable" />
                     <input type="checkbox" value="1" name="_printcart_pb_enable" id="_printcart_pb_enable" <?php checked($nbdpb_enable); ?> class="short" />
                 </span>
             </p>
-            <p class="nbo-form-field">
+            <p class="printcart-form-field">
                 <label>
                     <a href="<?php echo $link_edit_option; ?>" target="_blank" class="button">
                         <?php if ($option_id != 0) {
